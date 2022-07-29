7b50d805187695a63f1c41f79c6cc64eca6a9ac0
Alejololer <acarlos29112003@gmail.com>
2022-07-29 12:01:38 -0500

    commit

--- src/deliveryBin.cpp
+++ src/deliveryBin.cpp
@@ -94,12 +94,12 @@ int main()
                 system("cls");
                 SetColor(BLACK,WHITE);
                 cout << "Creador por:"<<endl
-                     <<"Sebastian Aisalla"<<endl<<"mario.aisalla@epn.edu.ec"<<endl
-                     <<"Carlos Aleman"<<endl<<"carlos.aleman@epn.edu.ec"<<endl
-                     <<"Luis Caiza"<<endl<<"luis.caiza03@epn.edu.ec"<<endl
-                     <<"Brithany Lopez"<<endl<<"mario.aisalla@epn.edu.ec"<<endl
-                     <<"Nicolas Reinoso"<<endl<<"mario.aisalla@epn.edu.ec"<<endl
-                     <<"Stalin Yungan"<<endl<<"stalin.yungan@epn.edu.ec"<<endl;
+                     <<"mario.aisalla@epn.edu.ec"<<endl<<"Sebastian Aisalla"<<endl
+                     <<"carlos.aleman@epn.edu.ec"<<endl<<"Carlos Aleman"<<endl
+                     <<"luis.caiza03@epn.edu.ec"<<endl<<"Luis Caiza"<<endl
+                     <<"mario.aisalla@epn.edu.ec"<<endl<<"Brithany Lopez"<<endl
+                     <<"mario.aisalla@epn.edu.ec"<<endl<<"Nicolas Reinoso"<<endl
+                     <<"stalin.yungan@epn.edu.ec"<<endl<<"Stalin Yungan"<<endl;
                 getch();
                 system("cls");
                 break;
